d66e35c65fb20da3f5ffd7aec3e63da08473074c
Junseo Rho <joonseor@gmail.com>
2025-05-26 14:23:55 +0900

    ㅁㄴㅇ

---
+++ Test2/src/studentManager/StudentManagerTest.java
@@ -0,0 +1,19 @@
+package studentManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StudentManagerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
